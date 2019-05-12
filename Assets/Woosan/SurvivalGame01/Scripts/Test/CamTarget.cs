0e3fd6727381051d4ed78fb4ca08e5727cdf3ad3
ChangKyun Ma <hihiduk119@gmail.com>
2019-05-12 16:02:36 +0900

    패키지 작업전 빽업.

--- Assets/Woosan/SurvivalGame01/Scripts/Test/CamTarget.cs
+++ Assets/Woosan/SurvivalGame01/Scripts/Test/CamTarget.cs
@@ -6,11 +6,13 @@ public class CamTarget : MonoBehaviour
 {
     [Header ("[따라다닐 타겟]")]
     public GameObject player;
-    [Header ("[따라다님 강도 [높을수록 빠르게 따라다님]]")]
+    [Header ("[따라다님 리커버 강도 [높을수록 빠르게 따라다님]]")]
     public float recoverStrong = 7.5f;
-    [Header ("[따라 다니는것 반대]")]
-    public bool reverse = false;
-    [Header("[역방향으로 따라다닐 타겟]")]
+    [Header("[전방 과 캐릭터 거리 [높을수록 거리 벌림]]")]
+    public float distance = 5.5f;
+    [Header ("[전방보며 활성화]")]
+    public bool lookAhead = true;
+    [Header("[전방을 내다보며 따라다닐 타겟]")]
     public GameObject reverseTarget;
     [Header ("[캠의 방향으로 조이스틱 조정하기 위해 사용]")]
     public Transform cam;
@@ -33,10 +35,10 @@ public class CamTarget : MonoBehaviour
         //해당 오브젝트가 비활성화 상태라면 움직이지 않음 [비용절약용 코드]
         if (!skinnedMeshRenderer.isVisible) return;
 
-        if(!reverse) {//타겟을 따라 다니는데 정상적으로 따라 다님
+        if(!lookAhead) {//기본 따라다니기
             //실제 러프를 거는 부분
             transform.localPosition = Vector3.Lerp(transform.localPosition, player.transform.localPosition, Time.deltaTime * recoverStrong);
-        } else {//타겟을 따라 다니든데 거꾸로 붙음
+        } else {//전방을 내다 보며 따라다니기
             h = UltimateJoystick.GetHorizontalAxis("Move");
             v = UltimateJoystick.GetVerticalAxis("Move");
 
@@ -44,8 +46,8 @@ public class CamTarget : MonoBehaviour
             desiredVelocity = v * camForward + h * cam.right;
 
             pos = player.transform.localPosition;
-            pos.z += desiredVelocity.z * 5;
-            pos.x += desiredVelocity.x * 5;
+            pos.z += desiredVelocity.z * distance;
+            pos.x += desiredVelocity.x * distance;
 
             //러프를 걸 타겟
             reverseTarget.transform.localPosition = pos;
@@ -53,7 +55,7 @@ public class CamTarget : MonoBehaviour
             //실제 러프를 거는 부분
             transform.localPosition = Vector3.Lerp(transform.localPosition, reverseTarget.transform.localPosition, Time.deltaTime * recoverStrong);
             //Debug.Log("h = [" + h + "] v = [" + v + "]" + "  pos = " + pos);
-            Debug.Log("this = " + transform.localPosition + " reverseTarget = " + reverseTarget.transform.localPosition);
+            //Debug.Log("this = " + transform.localPosition + " reverseTarget = " + reverseTarget.transform.localPosition);
         }
     }
 }

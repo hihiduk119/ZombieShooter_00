61b54058297617a293857f8d0d91d6ebdc16c1c7
ChangKyun Ma <hihiduk119@gmail.com>
2019-11-07 19:51:01 +0900

    카메라 워크 완성. 적 발견시 카메라워크 추가 필요함.

--- Assets/WoosanStudio/SurvivalGame01/Scripts/Test/FollowCameraTarget.cs
+++ Assets/WoosanStudio/SurvivalGame01/Scripts/Test/FollowCameraTarget.cs
@@ -12,17 +12,22 @@ namespace Woosan.SurvivalGame01
     {
         [Header("[따라다닐 타겟]")]
         public GameObject player;
-        [Header("[실제 캠이 따라다닐 타겟]")]
-        public GameObject followedCam;
+        [Header("[조이스틱 타겟]")]
+        public GameObject joystickTarget;
+        [Header("[조이스틱 타겟을 따라다나는 타겟]")]
+        public GameObject aheadTarget;
+        [Header("[캐릭터를 따라다나는 타겟]")]
+        public GameObject followTarget;
+        [Header("[FollowTarget 거리제한용 기준 타겟]")]
+        public GameObject distanceLockCenterTarget;
+        [Header("[FollowTarget 거리제한용 더미 타겟]")]
+        public GameObject distanceLockDummyTarget;
 
         [Header("[따라다님 리커버 강도 [높을수록 빠르게 따라다님]]")]
         public float recoverStrong = 7.5f;
         [Header("[전방 과 캐릭터 거리 [높을수록 거리 벌림]]")]
         public float distance = 5.5f;
-        [Header("[전방보며 활성화]")]
-        public bool lookAhead = true;
-        [Header("[전방을 내다보며 따라다닐 타겟]")]
-        public GameObject lookAheadTarget;
+
         [Header("[캠의 방향으로 조이스틱 조정하기 위해 사용]")]
         public Transform cam;
 
@@ -32,6 +37,9 @@ namespace Woosan.SurvivalGame01
         private float v;
         private Vector3 pos, desiredVelocity;
         private SkinnedMeshRenderer skinnedMeshRenderer;
+        //더미 좌표에 플레이어 좌료를 집어 넣기용. 범위를 벗어났을때 한번씩만 가능a
+        private bool setAbleDummy = true;
+
 
         private void Awake()
         {
@@ -44,34 +52,46 @@ namespace Woosan.SurvivalGame01
             //해당 오브젝트가 비활성화 상태라면 움직이지 않음 [비용절약용 코드]
             if (!skinnedMeshRenderer.isVisible) return;
 
-            if (!lookAhead)//기본 따라다니기
-            {
-             //실제 러프를 거는 부분followedCam.  
-                followedCam.transform.localPosition = Vector3.Lerp(followedCam.transform.localPosition, player.transform.localPosition, Time.deltaTime * recoverStrong);
+
+            //전방을 확인하기 위해 조이스틱 값을 가져옴
+            h = UltimateJoystick.GetHorizontalAxis("Move");
+            v = UltimateJoystick.GetVerticalAxis("Move");
+
+            //카메라와 조이스틱 포지션간의 왜곡을 보정하기 위해 방향 다시 계산.
+            camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
+            desiredVelocity = v * camForward + h * cam.right;
+
+            //플레이어의 좌표와 왜곡 보정계산된 방향 가속도를 현재 좌표에 적용.
+            pos = player.transform.localPosition;
+            pos.z += desiredVelocity.z * distance;
+            pos.x += desiredVelocity.x * distance;
+
+            //러프를 걸 타겟
+            joystickTarget.transform.localPosition = pos;
+
+            //실제 러프를 거는 followedCam.    
+            aheadTarget.transform.localPosition = Vector3.Lerp(aheadTarget.transform.localPosition, joystickTarget.transform.localPosition, Time.deltaTime * recoverStrong);
+
+            Debug.Log(Mathf.Abs(distanceLockCenterTarget.transform.position.z - player.transform.position.z));
+            //플레이어가 기준 좌표 보다 멀어지면 더미 타겟에 플레이어 현재 좌표를 집어넣고 followTarget 을 바꾼다
+            if (Mathf.Abs(distanceLockCenterTarget.transform.position.z - player.transform.position.z)  > 3) {
+                //Debug.Log(Vector3.Distance(distanceLockCenterTarget.transform.position, player.transform.position) + "커");
+                //더미 좌표에 플레이어 좌료를 집어 넣는다 [한번만 가능]
+                if(setAbleDummy) {
+                    setAbleDummy = false;
+                    distanceLockDummyTarget.transform.position = player.transform.position;
+                }
+
+                //followTarget 부분
+                followTarget.transform.localPosition = Vector3.Lerp(followTarget.transform.localPosition, distanceLockDummyTarget.transform.localPosition, Time.deltaTime * recoverStrong);
+            } else {
+                setAbleDummy = true;
+                //Debug.Log(Vector3.Distance(distanceLockCenterTarget.transform.position, player.transform.position) + "작아");
+                //followTarget 부분
+                followTarget.transform.localPosition = Vector3.Lerp(followTarget.transform.localPosition, player.transform.localPosition, Time.deltaTime * recoverStrong);
             }
-            else //전방을 내다 보며 따라다니기
-            {
-                //전방을 확인하기 위해 조이스틱 값을 가져옴
-                h = UltimateJoystick.GetHorizontalAxis("Move");
-                v = UltimateJoystick.GetVerticalAxis("Move");
 
-                //카메라와 조이스틱 포지션간의 왜곡을 보정하기 위해 방향 다시 계산.
-                camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
-                desiredVelocity = v * camForward + h * cam.right;
-
-                //플레이어의 좌표와 왜곡 보정계산된 방향 가속도를 현재 좌표에 적용.
-                pos = player.transform.localPosition;
-                pos.z += desiredVelocity.z * distance;
-                pos.x += desiredVelocity.x * distance;
-
-                //러프를 걸 타겟
-                lookAheadTarget.transform.localPosition = pos;
-
-                //실제 러프를 거는 followedCam.    
-                followedCam.transform.localPosition = Vector3.Lerp(followedCam.transform.localPosition, lookAheadTarget.transform.localPosition, Time.deltaTime * recoverStrong);
-                //Debug.Log("h = [" + h + "] v = [" + v + "]" + "  pos = " + pos);
-                //Debug.Log("this = " + transform.localPosition + " lookAheadTarget = " + lookAheadTarget.transform.localPosition);
-            }
+            //Debug.Log(Vector3.Dot(followTarget.transform.position, player.transform.localPosition));
         }
     }
 }

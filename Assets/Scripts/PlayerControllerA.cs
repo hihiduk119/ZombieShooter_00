46ced649a7d3047f8afab1d064eabc442e7ae305
ChangKyun Ma <hihiduk119@gmail.com>
2019-11-11 12:23:55 +0900

    일단 빽업

---
+++ Assets/Scripts/PlayerControllerA.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerA : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed = 5;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	void FixedUpdate () {
+		//float moveHorizontal = Input.GetAxis ("Horizontal");
+		//float moveVertical = Input.GetAxis ("Vertical");
+
+        float moveHorizontal = UltimateJoystick.GetHorizontalAxis("Move");
+        float moveVertical = UltimateJoystick.GetVerticalAxis("Move");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}

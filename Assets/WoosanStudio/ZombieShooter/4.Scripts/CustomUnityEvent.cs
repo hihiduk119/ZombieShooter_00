b83ebd4abe4a6ba47c64ff20e339322a69a6e65c
ChangKyun Ma <hihiduk119@gmail.com>
2019-12-29 16:51:25 +0900

    OOP 원칙 해서 새로 작업하기 위해 초기화함.

--- Assets/WoosanStudio/ZombieShooter/4.Scripts/CustomUnityEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.Events;
-using System;
-
-//에임 사거리 용 이벤트
-[Serializable]
-public class TransformUnityEvent : UnityEvent<Transform> {}
--- Assets/WoosanStudio/ZombieShooter/4.Scripts/Enums.cs
+++
@@ -1,6 +0,0 @@
-﻿
-public enum ZombieKinds {
-    WeakZombie,
-    Zombie,
-    StrongZombie,
-}

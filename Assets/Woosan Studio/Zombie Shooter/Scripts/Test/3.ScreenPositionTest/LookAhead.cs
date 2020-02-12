c7dd3c8991186949a835ef2efc6505e045881dbe
ChangKyun Ma <hihiduk119@gmail.com>
2020-02-12 21:12:43 +0900

    다시 커밋

---
+++ Assets/Woosan Studio/Zombie Shooter/Scripts/Test/3.ScreenPositionTest/LookAhead.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAhead : MonoBehaviour
+{
+    public Transform Target;
+    public float Speed = 100f;
+
+    IEnumerator _corLookAhead;
+
+    public void Look(Vector3 target)
+    {
+        transform.LookAt(target);
+        //if (_corLookAhead != null) StopCoroutine(_corLookAhead);
+        //StartCoroutine()
+    }
+
+    private void Update()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rot.x = 0;
+        transform.localRotation = Quaternion.Euler(rot);
+    }
+
+    IEnumerator CorLookAhead()
+    {
+        while(true)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Woosan Studio/Zombie Shooter/Scripts/Test/3.ScreenPositionTest/MoveScreenPointToRayPosition.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+[System.Serializable]
+public class UpdatePositionEvent : UnityEvent<Vector3> { }
+
+public class MoveScreenPointToRayPosition : MonoBehaviour
+{
+    //카메라 세팅부분
+    public Camera Camera;
+
+    private bool Activate { get; set; }
+
+    private Vector3 _point = Vector3.zero;
+
+    public UpdatePositionEvent UpdatePositionEvent = new UpdatePositionEvent();
+
+    //캐쉬
+    Ray _ray;
+    RaycastHit _hit;
+
+    private void OnDisable()
+    {
+        Activate = false;
+    }
+
+    private void OnEnable()
+    {
+        Activate = true;
+    }
+
+    private void Move()
+    {
+        transform.position = _point;
+    }
+
+    /// <summary>
+    /// 레이에 맞은 부분을 가져옴
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 GetHitPoint()
+    {
+        return _point;
+    }
+
+    void Update()
+    {
+        if (!Activate) return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            _ray = Camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(_ray, out _hit))
+            {
+                _point = _hit.point;
+                //_point.y += 0.25f;
+                UpdatePositionEvent.Invoke(_point);
+                Move();
+            }
+        }
+    }
+}
+

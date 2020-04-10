d89fd4bf8916d2fb86de80d66d397ae5fb3b9cc6
ChangKyun Ma <hihiduk119@gmail.com>
2020-04-10 17:21:21 +0900

    [완료] 1. 공습 이펙트 적용 완료.
    [버그]
    
    [참고]
    1. 공습 데미지 적용 해야함.
    2. 몬스터가 피해를 받게 만들어야 함.
    
    [작업예정]
    0. 다양한 형태의 몬스터 필요.
     -  라이더 형
     - 땅굴형
     - 원거리 공격형
     - 거인형
     - 중갑 화염 방사형
    1. DoDie를 Character와 통합할지 말지 결정해야 할듯. [리팩토링] => 통합시 이벤트 부분의 변경될 가능성 있음.
    2. 에니메이션 인터페이스 구체화 작업 필요.
    3. 몬스터 체력 실제 적용되게 해야함.
    - 몬스터 사망시 텍스트 데이미 UI 메니저에 해당 UI 반환시켜야함.
    - 몬스터 사망 후 떠오름 끝난다음 초기화 작업 필요.-> 재활용 하지 않고 삭제 예정.
    4. 바리케이트 데미지 표시 및 데미지 받았을때 해당 효과 호출되게 만들기.

---
+++ Assets/Woosan Studio/Zombie Shooter/Scripts/Explosion/ExplosionFactory.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace WoosanStudio.ZombieShooter
+{
+    /// <summary>
+    /// 화면 전체에 폭발을 만듬.
+    /// </summary>
+    public class ExplosionFactory : MonoBehaviour
+    {
+        //폭발 객체의 종류
+        public enum ExplosionType
+        {
+            BlueDemonFire,
+            FunkyMixJokerFire,
+            GreenWildFire,
+            PurpleDarkFire,
+            RedOriginalFire,
+            RedOriginalFireNoSmoke,
+            ClassicHeavyBomb,
+        }
+
+        [Header("[폭발 프리팹 세팅 리스튼")]
+        public List<ExplosionSetting> settings = new List<ExplosionSetting>();
+
+        //폭발 포지션
+        private List<Transform> points;
+
+        private void Awake()
+        {
+            points = new List<Transform>(GetComponentsInChildren<Transform>());
+
+            points.ForEach(value => Debug.Log(value.name));
+        }
+
+        public void Show(ExplosionSetting setting)
+        {
+            //points.ForEach(value =>
+            //{
+            //    GameObject clone = Instantiate(setting.prefabs[Random.Range(0, setting.prefabs.Length)]);
+            //    clone.transform.position = value.position;
+            //});
+
+            StartCoroutine(SequentialShow(setting));
+        }
+
+        IEnumerator SequentialShow(ExplosionSetting setting)
+        {
+            int index = 0;
+            while(points.Count > index)
+            {
+                int rand = Random.Range(1, setting.prefabs.Length);
+                GameObject clone = Instantiate(setting.prefabs[rand]);
+                //GameObject clone2 = Instantiate(settings[6].prefabs[rand]);
+
+                clone.transform.position = points[index].position;
+                //clone2.transform.position = points[index].position;
+
+                yield return new WaitForSeconds(0.05f);
+                index++;
+            }
+        }
+
+        #region [-TestCode]
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.A)){
+                Show(settings[(int)ExplosionType.RedOriginalFire]);
+            }   
+        }
+        #endregion
+    }
+}

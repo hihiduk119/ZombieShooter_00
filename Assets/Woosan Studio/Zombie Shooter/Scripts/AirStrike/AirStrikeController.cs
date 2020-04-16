d916bf0d27a2866eb7352c2c43f0dd823c6de010
ChangKyun Ma <hihiduk119@gmail.com>
2020-04-16 16:29:16 +0900

    같은 파일 미싱 부분 재업

---
+++ Assets/Woosan Studio/Zombie Shooter/Scripts/AirStrike/AirStrikeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+namespace WoosanStudio.ZombieShooter
+{
+    /// <summary>
+    /// 에어 스트라이크를 연출 전체룰 컨트롤 함.
+    /// </summary>
+    public class AirStrikeController : MonoBehaviour
+    {
+        public Transform StartPoint;
+        public Transform EndPoint;
+        public Transform Target;
+
+        public void SetPoint(Transform startPoint, Transform endPoint)
+        {
+            this.StartPoint = startPoint;
+            this.EndPoint = endPoint;
+        }
+
+        /// <summary>
+        /// 실제 연출 수행
+        /// </summary>
+        public void Run()
+        {
+            Target.position = StartPoint.position;
+
+            Target.DOMove(EndPoint.position, 1.5f).SetEase(Ease.Linear).OnComplete(() => {
+                Debug.Log("OnComplete");
+                ExplosionFactory.Instance.TestRun();
+            });
+        }
+
+
+        #region [-TestCode]
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Run();
+            }
+        }
+        #endregion
+    }
+}

15174439f5f606b4897c63c5b3cd12364529800b
ChangKyun Ma <hihiduk119@gmail.com>
2020-05-06 15:45:28 +0900

    가속도 센서 적용작업중 빽업. 모바일 디버거 추가함.

---
+++ Assets/Woosan Studio/Zombie Shooter/Scripts/Input/Sensors/Accelerometer/Accelerometer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WoosanStudio.Common
+{
+    public class Accelerometer : MonoBehaviour
+    {
+        Rigidbody rigid;
+
+        private void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        //private void Update()
+        //{
+        //    rigid.AddForce(Input.acceleration);
+        //}
+
+
+        //This is a legacy function, check out the UI section for other ways to create your UI
+        void OnGUI()
+        {
+            //Output the rotation rate, attitude and the enabled state of the gyroscope as a Label
+            GUI.Label(new Rect(500, 300, 200, 40), "acceleration " + Input.acceleration);
+
+            GUI.Label(new Rect(500, 350, 200, 40), "x = " + Input.acceleration.x);
+        }
+    }
+}

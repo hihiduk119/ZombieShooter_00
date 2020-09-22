46620012ec57bcb9a20afb17779b1baaaa5214da
ChangKyun Ma <hihiduk119@gmail.com>
2020-09-22 14:20:15 +0900

    [완료] 1. MonsterSpawnScheduleManager. 작업중. 라운드별 작업 중. 라운드 연동 완료.[80%]. 2. ExplodingProjectile.cs에서 몬스터에게 주는 데미지 계산식 작업중4. 3. CardManager.cs AddCard 	부분 오류 수정완료.
    [버그]
    0. 전에 없던 카드 셀렉터 창 시작시 에러 발생.
    1.버그창에 에러 표시문제 해결
    - 블랫 프리팹에 DoDestroy 스크립트 빠져서 생기는 에러
    
    [참고]
    0. 플레이어 콜라이더와 바닥 콜라이더가 걸려서 자꾸 멈추는 느낌 있었음. 생성시 오브젝트 높이 고려해서 생성 시켜야함.
    1. 플레이어 리포지션시 FollowCam 역시 같이 좌표로 이동 시켜야 함.
    1. 데미지 UI의 경우 처음부터 맵 상에 존재하는 최대 몬스터 제한이 필요 할듯함. 느려짐 발생 가능성 있음. 레벨 디자인 하면서 조정이 필요.
    
    [작업예정]
    0. 재장선시 리로드 UI 메시지 데미지 창에 출력 -> 사운드 같이
    0. 스테이지 정상호출 부가 작업 해야함. - 씬 이동시 플레이어 디스에이블.
    0. 프랍 오브젝트 static 비활성화 시켜야함.
    1. 몬스터 활성화 및 포지션 재조정.
    2. 몬스터 죽었을때 데드 연출 변경필요.
    3. 몬스터 생성시 에러 확인.

--- Assets/WoosanStudio/Zombie Shooter/Scripts/Managers/CardManager.cs
+++ Assets/WoosanStudio/Zombie Shooter/Scripts/Managers/CardManager.cs
@@ -18,9 +18,6 @@ namespace WoosanStudio.ZombieShooter
         [Header("[[테스트용 삭제예정] 로비 선택 카드 => 테스트 용이며 직접 사용하지 않는다]")]
         public List<CardSetting> CardSettingsReference = new List<CardSetting>();
 
-        //[Header("[[테스트용 삭제예정] cardSettings의 복제 => 실제 사용됨]")]
-        //public List<CardSetting> CardSettingsClone;
-
         [Header("[테스트용을 복사한 카드 => [삭제예정]]")]
         public List<CardSetting> CardsPlayerOnClicked;
 
@@ -49,7 +46,7 @@ namespace WoosanStudio.ZombieShooter
 
 
         /// <summary>
-        /// 추가 카드에 따라 해당 카드와 같은 타입 모든 카드 비활성화
+        /// 해당 범위의 카드를 비활성화 시킴
         /// </summary>
         /// <param name="cards"></param>
         /// <param name="cardRange"></param>
@@ -60,6 +57,7 @@ namespace WoosanStudio.ZombieShooter
                 if (cardRange[0] <= (int)value.Type && (int)value.Type < cardRange[1])
                 {
                     value.IsActivate = false;
+                    Debug.Log("value name = " + value.name);
                 }
             });
         }
@@ -68,10 +66,11 @@ namespace WoosanStudio.ZombieShooter
         /// 카드 추가
         /// * 카드 추가시 중첩 확인 및 없을시 추가.
         /// * 이미 존재 한다면 중첩 카운트 증가
+        /// * 활성 카드의 프로퍼티는 따로 모음.
         /// </summary>
         public void AddCard(CardSetting cardSetting)
         {
-            Debug.Log("1 카드이름 = " + cardSetting.name + " ["+ cardSetting.IsActivate + "]");
+            //Debug.Log("1 카드이름 = " + cardSetting.name + " ["+ cardSetting.IsActivate + "]");
             //추가시 해당 카드는 무조건 활성 상태
             cardSetting.IsActivate = true;
 
@@ -79,20 +78,28 @@ namespace WoosanStudio.ZombieShooter
             //cardSetting.Properties.ForEach(value => SelectedCardProperties.Add(Instantiate<CardProperty>(value)));
 
             //분류가 안돼 이부분 재 정의 필요.
+            //추가 카드의 100-200 사이의 카드 - GunType
+            if(100 <= (int)cardSetting.Type && (int)cardSetting.Type < 200)
+            {
+                //100-200 사이의 카드 타입 모두 비활성
+                DeactiveByCardType(this.HasCards, new int[] { 100, 200 });
+            }
 
-            //추가 카드가 총 타입 변환이라면 기존 총 타입 모두 비활성
-            DeactiveByCardType(this.HasCards, new int[] { 100, 200});
+            //추가 카드의 200-300 사이의 카드라면 - AmmoType
+            if (200 <= (int)cardSetting.Type && (int)cardSetting.Type < 300)
+            {
+                //추가 카드가 탄약 타입 변환이라면 기존 탄약 타입 모두 비활성
+                DeactiveByCardType(this.HasCards, new int[] { 200, 300 });
+            }
 
-            //추가 카드가 탄약 타입 변환이라면 기존 탄약 타입 모두 비활성
-            DeactiveByCardType(this.HasCards, new int[] { 200, 300 });
+            //추가 카드의 300-400 사이의 카드라면 - CharacterType
+            if (300 <= (int)cardSetting.Type && (int)cardSetting.Type < 400)
+            {
+                //추가 카드가 캐릭터 타입 변환이라면 기존 캐릭터 타입 모두 비활성
+                DeactiveByCardType(this.HasCards, new int[] { 300, 400 });
+            }
 
-            //추가 카드가 캐릭터 타입 변환이라면 기존 캐릭터 타입 모두 비활성
-            DeactiveByCardType(this.HasCards, new int[] { 300, 400 });
-
-            //Debug.Log("추가중 = " + cardSetting.TypeByCharacter.ToString());
-
-            Debug.Log("2 카드이름 = " + cardSetting.name + " [" + cardSetting.IsActivate + "]");
-
+            //기존 가지고 있는 카드와 비교하여 없으면 stack 카운트올리고 없으면 추가함
             bool ableAdd = true;
             for (int i = 0; i < HasCards.Count; i++)
             {
@@ -106,22 +113,23 @@ namespace WoosanStudio.ZombieShooter
                     //스택카운터 증가
                     HasCards[i].StackCount++;
                     //추가 불가 플래그 
-                    ableAdd = false;   
+                    ableAdd = false;
+                    //해당 카드 활성화
+                    HasCards[i].IsActivate = true;
                 }
             }
 
-            Debug.Log("3 카드이름 = " + cardSetting.name + " [" + cardSetting.IsActivate + "]");
-
-            //추가 가능 플래그라면 해당 카드 추가
+            //추가 가능 플래그라면 
             if (ableAdd) {
+                //해당 카드 활성화
+                cardSetting.IsActivate = true;
+                //해당 카드 추가
                 HasCards.Add(cardSetting);
                 //Debug.Log("추가됨 = " + cardSetting.TypeByCharacter.ToString());
             }
 
+            //IsActivate 카드의 프로퍼티만 중첩횟수만큼 ActivatedCardProperties에 추가함
             //기존 프로퍼티 모두 삭제 다시 넣기
-            //*이때 isActivate만 넣음
-            Debug.Log("4 카드이름 = " + cardSetting.name + " [" + cardSetting.IsActivate + "]");
-
             if (ActivatedCardProperties == null) ActivatedCardProperties = new List<CardProperty>();
 
             //안에 있던 모든 프로퍼티 삭제
@@ -135,7 +143,7 @@ namespace WoosanStudio.ZombieShooter
             //활성 상태인 카드의 프로퍼티만 넣기
             //*중첩(stack)이면 중첩만큼 넣어야함
             HasCards.ForEach(value => {
-                Debug.Log("HasCard = " + value.name + " [" + value.IsActivate + "]");
+                //Debug.Log("HasCard = " + value.name + " [" + value.IsActivate + "]");
                 if (value.IsActivate)//활성 상태인 것만 넣기
                 {
                     for (int stack = 0; stack <= value.StackCount; stack++) //중첩 만큼 추가로 넣음
@@ -149,8 +157,6 @@ namespace WoosanStudio.ZombieShooter
                     }
                 }
             });
-
-            Debug.Log("5 카드이름 = " + cardSetting.name + " [" + cardSetting.IsActivate + "]");
         }
 
 
@@ -162,6 +168,7 @@ namespace WoosanStudio.ZombieShooter
             {
                 for (int i = 0; i < CardsPlayerOnClicked.Count; i++)
                 {
+                    Debug.Log("===============> 추가할 카드 = " + CardsPlayerOnClicked[i].name +" <==============");
                     AddCard(CardsPlayerOnClicked[i]);
 
                     //CardsPlayerOnClicked.Clear();

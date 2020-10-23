3a4ad9652c91fb44ad35951e903014ef90f16531
ChangKyun Ma <hihiduk119@gmail.com>
2020-10-23 16:13:31 +0900

    UI 캐릭터 인포 데이터 적용 완료.

--- Assets/WoosanStudio/UI Dev/Scripts/UI/MVP/UIPlayerInfoView.cs
+++ Assets/WoosanStudio/UI Dev/Scripts/UI/MVP/UIPlayerInfoView.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 namespace WoosanStudio.ZombieShooter
 {
     /// <summary>
-    /// 
+    /// 플레이어 정보 표시
+    /// *플레이어 정보 창을 여는 역활도 함.
     /// *MPV 모델
     /// </summary>
     public class UIPlayerInfoView : MonoBehaviour
     {
         public UnityEngine.UI.Image Image;
 
+        //인포 뷰어 열때 사용하기 위해 저장
+        public UIPlayerPresenter.InfoViewData infoViewData;
+
         /// <summary>
         /// 캐릭터 이미지 업데이트
         /// </summary>
         /// <param name="sprite"></param>
-        public void UpdateImage(Sprite sprite)
+        public void UpdatePlayerInfo(UIPlayerPresenter.InfoViewData data)
         {
-            Image.sprite = sprite;
+            //받은 데이터 일단 저장
+            infoViewData = data;
+
+            Image.sprite = infoViewData.Image;
         }
     }
 }
---
+++ Assets/WoosanStudio/UI Dev/Scripts/UI/Popup/UIPlayerInfoPopupView.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+using System.Text;
+
+namespace WoosanStudio.ZombieShooter
+{
+    /// <summary>
+    /// UI 플레이어 인포 팝업
+    /// 
+    /// *MVC패턴
+    /// </summary>
+    public class UIPlayerInfoPopupView : MonoBehaviour
+    {
+        [Header("[캐릭터 이미지]")]
+        public Image Image;
+        [Header("[캐릭터 이름]")]
+        public Text Name;
+        [Header("[캐릭터 설멍]")]
+        public Text Description;
+        [Header("[캐릭터 플레이타임]")]
+        public Text PlayTime;
+        [Header("[캐릭터가 사냥한 몬스터 수]")]
+        public Text HuntedMonster;
+
+        private StringBuilder stringBuilder = new StringBuilder();
+
+        private void Start()
+        {
+            //시작시 씬에서 UIPlayerInfoView 찾아서 데이터 가져옴
+            UIPlayerInfoView infoView = GameObject.FindObjectOfType<UIPlayerInfoView>();
+            //가져온 데이터로 정보 업데이트
+            UpdateInfo(infoView.infoViewData);
+        }
+
+        /// <summary>
+        /// 정보 업데이트
+        /// </summary>
+        public void UpdateInfo(UIPlayerPresenter.InfoViewData data)
+        {
+            //일단 초기화
+            stringBuilder.Clear();
+            //캐릭터 이미지 넣기
+            Image.sprite = data.Image;
+            //캐릭터 이름 넣기
+            Name.text = data.Name;
+            //캐릭터 정보 스트링 하나로 합치기
+            for (int i = 0; i < data.Descripsions.Count; i++)
+            {
+                //정보 넣기
+                stringBuilder.Append(data.Descripsions[i]);
+                //개행
+                stringBuilder.AppendLine();
+            }
+            //합친 캐릭터 정보 넣기
+            Description.text = stringBuilder.ToString();
+            //해당 캐릭터 플레이 타임 넣기
+            PlayTime.text = data.PlayTime.ToString();
+            //해당 캐릭터 사냥한 몬스터 숫자 넣기
+            HuntedMonster.text = data.HuntedMonster.ToString();
+        }
+    }
+}

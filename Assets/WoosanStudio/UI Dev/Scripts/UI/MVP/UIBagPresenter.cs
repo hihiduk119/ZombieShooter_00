9f3f8d8bca5667655837d7c8ce5ac68619e944ce
ChangKyun Ma <hihiduk119@gmail.com>
2021-01-29 16:09:35 +0900

    현재 획득한 아이템 정보 팝업 작업 완료.

---
+++ Assets/WoosanStudio/UI Dev/Scripts/UI/MVP/UIBagPresenter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WoosanStudio.ZombieShooter.UI
+{
+    /// <summary>
+    /// 백 프리젠트
+    /// *MVP 모델
+    /// </summary>
+    public class UIBagPresenter : MonoBehaviour
+    {
+        [Header("[MVP View]")]
+        public UIBagView View;
+
+        /// <summary>
+        /// 정보 업데이트
+        /// </summary>
+        public void UpdateInfo()
+        {
+            View.UpdateInfo("0", "0", "0");
+        }
+    }
+}
---
+++ Assets/WoosanStudio/UI Dev/Scripts/UI/MVP/UIBagView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+namespace WoosanStudio.ZombieShooter.UI
+{
+    /// <summary>
+    /// 백 뷰
+    /// *MVP 모델
+    /// </summary>
+    public class UIBagView : MonoBehaviour
+    {
+        [Header("[획득 경험치]")]
+        public Text Exp;
+
+        [Header("[획득 코인]")]
+        public Text Coin;
+
+        [Header("[획득 젬]")]
+        public Text Gem;
+
+        /// <summary>
+        /// 정보 업데이트
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <param name="coin"></param>
+        /// <param name="gem"></param>
+        public void UpdateInfo(string exp, string coin, string gem)
+        {
+            Exp.text = "+" + exp;
+            Coin.text = "+" + coin;
+            Gem.text = "+" + gem;
+        }
+    }
+}

e49a38f9d5702293efa095e98f7fb836aa7dd691
ChangKyun Ma <hihiduk119@gmail.com>
2021-01-29 16:49:51 +0900

    아웃라인 시스템 적용 완료.

---
+++ Assets/Plugins/Easy performant outline/Scripts/OutlineCustomPass.cs
@@ -0,0 +1,163 @@
+﻿using EPOOutline;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+#if HDRP_OUTLINE
+using UnityEngine.Rendering.HighDefinition;
+
+[VolumeComponentMenu("EPO/Outline")]
+public class OutlineCustomPass : CustomPass
+{
+    private List<Outlinable> tempOutlinables = new List<Outlinable>();
+
+    private Queue<OutlineParameters> pool = new Queue<OutlineParameters>();
+    
+    private Queue<OutlineParameters> parametersInUse = new Queue<OutlineParameters>();
+
+    private List<Outliner> outliners = new List<Outliner>();
+
+    [SerializeField]
+    [HideInInspector]
+    private Camera lastSelectedCamera = null;
+
+    protected override void Execute(ScriptableRenderContext renderContext, CommandBuffer cmd, HDCamera hdCamera, CullingResults cullingResult)
+    {
+        var camera = hdCamera.camera;
+
+        if (outliners == null)
+            outliners = new List<Outliner>();
+
+        outliners.Clear();
+
+#if UNITY_EDITOR
+        if (camera.cameraType == CameraType.SceneView)
+        {
+            if (lastSelectedCamera == null)
+            {
+                foreach (var obj in UnityEditor.Selection.gameObjects)
+                {
+                    lastSelectedCamera = obj.GetComponent<Camera>();
+                    if (lastSelectedCamera != null)
+                        lastSelectedCamera.GetComponents(outliners);
+
+                    if (outliners.Count != 0)
+                        break;
+                    else
+                        lastSelectedCamera = null;
+                }
+            }
+
+            outliners.Clear();
+            if (lastSelectedCamera != null)
+                lastSelectedCamera.GetComponents(outliners);
+        }
+        else
+#endif
+            camera.GetComponents(outliners);
+
+        foreach (var outliner in outliners)
+            UpdateOutliner(renderContext, camera, outliner);
+
+        if (parametersInUse != null)
+        {
+            foreach (var param in parametersInUse)
+                pool.Enqueue(param);
+
+            parametersInUse.Clear();
+        }
+    }
+
+    private void UpdateOutliner(ScriptableRenderContext renderContext, Camera camera, Outliner outlineEffect)
+    {
+        if (outlineEffect == null || !outlineEffect.enabled)
+            return;
+
+        if (pool == null)
+            pool = new Queue<OutlineParameters>();
+
+        if (parametersInUse == null)
+            parametersInUse = new Queue<OutlineParameters>();
+
+        if (pool.Count == 0)
+            pool.Enqueue(new OutlineParameters());
+
+        var parameters = pool.Dequeue();
+        parametersInUse.Enqueue(parameters);
+
+        parameters.CheckInitialization();
+
+        parameters.OutlineLayerMask = outlineEffect.OutlineLayerMask;
+
+        outlineEffect.UpdateSharedParameters(parameters, camera, camera.cameraType == CameraType.SceneView);
+        Outlinable.GetAllActiveOutlinables(parameters.Camera, parameters.OutlinablesToRender);
+        RendererFilteringUtility.Filter(parameters.Camera, parameters);
+
+        parameters.Buffer.EnableShaderKeyword("EPO_HDRP");
+
+        if (outlineEffect.RenderingStrategy == OutlineRenderingStrategy.Default)
+        {
+            outlineEffect.UpdateSharedParameters(parameters, camera, camera.cameraType == CameraType.SceneView);
+
+            RTHandle colorTarget;
+            RTHandle depthTarget;
+            GetCameraBuffers(out colorTarget, out depthTarget);
+
+            parameters.Target = colorTarget;
+            parameters.DepthTarget = depthTarget;
+
+            parameters.TargetWidth = colorTarget.referenceSize.x;
+            parameters.TargetHeight = colorTarget.referenceSize.y;
+            parameters.Antialiasing = colorTarget.rt.antiAliasing;
+
+            parameters.Prepare();
+
+            OutlineEffect.SetupOutline(parameters);
+
+            renderContext.ExecuteCommandBuffer(parameters.Buffer);
+        }
+        else
+        {
+            if (tempOutlinables == null)
+                tempOutlinables = new List<Outlinable>();
+
+            tempOutlinables.Clear();
+            tempOutlinables.AddRange(parameters.OutlinablesToRender);
+
+            foreach (var outlinable in tempOutlinables)
+            {
+                outlineEffect.UpdateSharedParameters(parameters, camera, camera.cameraType == CameraType.SceneView);
+
+                RTHandle colorTarget;
+                RTHandle depthTarget;
+                GetCameraBuffers(out colorTarget, out depthTarget);
+
+                parameters.Target = colorTarget;
+                parameters.DepthTarget = depthTarget;
+
+                parameters.TargetWidth = colorTarget.referenceSize.x;
+                parameters.TargetHeight = colorTarget.referenceSize.y;
+                parameters.Antialiasing = colorTarget.rt.antiAliasing;
+
+                parameters.OutlinablesToRender.Clear();
+                parameters.OutlinablesToRender.Add(outlinable);
+
+                parameters.BlitMesh = null;
+
+                parameters.Prepare();
+
+                OutlineEffect.SetupOutline(parameters);
+                renderContext.ExecuteCommandBuffer(parameters.Buffer);
+
+                parameters.Buffer.Clear();
+            }
+
+            parameters.MeshPool.ReleaseAllMeshes();
+        }
+        
+        parameters.Buffer.Clear();
+    }
+}
+#endif

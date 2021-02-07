c86f29181a1060d5e990e5f896d1052d2d78d39d
ChangKyun Ma <hihiduk119@gmail.com>
2021-02-07 18:02:35 +0900

    네임드 몬스터 저항 작업중. 기존 네임드 컨트롤러 제거해야함.

---
+++ Assets/WoosanStudio/UI Dev/Scripts/UI/MVP/UINamedMonsterBarPresenter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+namespace WoosanStudio.ZombieShooter.UI
+{
+    /// <summary>
+    /// 네임드 바 프리젠터
+    /// *MVP 모델
+    /// </summary>
+    public class UINamedMonsterBarPresenter : MonoBehaviour
+    {
+        [Header("[MVP View]")]
+        public UINamedMonsterBarView View;
+
+        /// <summary>
+        /// 활성 비활성
+        /// </summary>
+        public void SetActivate(bool value)
+        {
+            View.SetActivate(value);
+        }
+    }
+}
---
+++ Assets/WoosanStudio/UI Dev/Scripts/UI/MVP/UINamedMonsterBarView.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WoosanStudio.ZombieShooter.UI
+{
+    /// <summary>
+    /// 네임드 바 뷰어
+    /// *MVP 모델
+    /// </summary>
+    public class UINamedMonsterBarView : MonoBehaviour
+    {
+        //캐쉬용
+        private CanvasGroup canvasGroup;
+
+        [Header("[아이템이 이동할 피벗]")]
+        public Transform[] Pivots;
+
+        //public 
+        private void Awake()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        /// <summary>
+        /// 활성 또는 비활성
+        /// </summary>
+        public void SetActivate(bool value)
+        {
+            if (value)
+            {
+                canvasGroup.alpha = 0f;
+                canvasGroup.DOFade(1f, 0.25f);
+            }
+            else
+            {
+                canvasGroup.alpha = 1f;
+                canvasGroup.DOFade(0f, 0.25f);
+            }
+        }
+    }
+}
